8a2ecd63d12c8c317bb2025702f6463e141607dd
Jeremy Corbett <jeremy.lael@gmail.com>
2011-08-13 19:51:20 -0400

    Jesus! git instead of get again

--- features/step_definitions/processes_steps.rb
+++ features/step_definitions/processes_steps.rb
@@ -1,8 +1,9 @@
 Given /^A process this is running in$/ do
-    pending # express the regexp above with the code you wish you had
+  #if we get here, it's true
 end
 
 Then /^the brave programmer can determine the pid$/ do
-    pending # express the regexp above with the code you wish you had
+  #pid will be greater than init's of 1
+  TLPI::Six.gitpid.should > 1
 end
 
