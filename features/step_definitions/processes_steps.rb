001cc97c904eb745779bc03a713e5fac56bd4d95
Jeremy Corbett <jeremy.lael@gmail.com>
2011-08-13 19:21:53 -0400

    Pending steps

---
+++ features/step_definitions/processes_steps.rb
@@ -0,0 +1,8 @@
+Given /^A process this is running in$/ do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then /^the brave programmer can determine the pid$/ do
+    pending # express the regexp above with the code you wish you had
+end
+
