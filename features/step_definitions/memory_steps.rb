b2a337e3604ca926cb0b894d8d88726db3cc3185
Jeremy Corbett <jeremy.lael@gmail.com>
2011-08-13 23:07:04 -0400

    Now pending

--- features/step_definitions/memory_steps.rb
+++ features/step_definitions/memory_steps.rb
@@ -7,3 +7,7 @@ Then /^I get a pointer$/ do
   @returned.instance_of?(FFI::Pointer).should be
 end
 
+Then /^I can call free on it$/ do
+    pending # express the regexp above with the code you wish you had
+end
+
