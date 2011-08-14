1f62ab3506292ffb5d30f9996b12adc87f4d1e93
Jeremy Corbett <jeremy.lael@gmail.com>
2011-08-13 23:04:48 -0400

    malloc is passing tests

--- features/step_definitions/memory_steps.rb
+++ features/step_definitions/memory_steps.rb
@@ -1,10 +1,9 @@
 When /^I call malloc$/ do
   @returned = TLPI::Seven.malloc 1024
-  require 'pry'
-  binding.pry
 end
 
 Then /^I get a pointer$/ do
-    pending # express the regexp above with the code you wish you had
+  @returned.should be
+  @returned.instance_of?(FFI::Pointer).should be
 end
 
