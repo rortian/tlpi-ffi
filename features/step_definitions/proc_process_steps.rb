a257ec1212b1b553b9602eaf302057a6d3b5afc4
Jeremy Corbett <jeremy.lael@gmail.com>
2011-08-13 20:25:46 -0400

    Pending definitions

---
+++ features/step_definitions/proc_process_steps.rb
@@ -0,0 +1,9 @@
+
+When /^I look at \/proc\/self\/status$/ do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should have some info about myself$/ do
+    pending # express the regexp above with the code you wish you had
+end
+
