5d7ac4184c929ea0324cdf2a42b64d315bd55420
Jeremy Corbett <jeremy.lael@gmail.com>
2011-08-13 20:54:14 -0400

    Getting some lines processed

--- lib/proc/proc.rb
+++ lib/proc/proc.rb
@@ -1,6 +1,8 @@
 module TLPI::Proc
   class Process
 
+    attr_reader :name,:state,:tgid
+
     def initialize(pid = 'self')
       @status_path = "/proc/#{pid}/status"
       check_status
@@ -9,9 +11,14 @@ module TLPI::Proc
     def check_status
       status = File.open @status_path
       status.readlines.each do |line|
+        spl = line.split
         case line
-        when ''
-
+        when /Name:/
+          @name = spl[1]
+        when /State:/
+          @state = line
+        when /Tgid:/
+          @tgid = spl[1]
         else
           puts line
         end
