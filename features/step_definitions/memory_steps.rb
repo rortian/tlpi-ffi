3d6b375bb0a91805bdfffc084c83c54185529ce6
Jeremy Corbett <jeremy.lael@gmail.com>
2011-08-13 22:57:40 -0400

    Now pending

---
+++ features/step_definitions/memory_steps.rb
@@ -0,0 +1,8 @@
+When /^I call malloc$/ do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then /^I get a pointer$/ do
+    pending # express the regexp above with the code you wish you had
+end
+
