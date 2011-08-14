0c605d9d7e833dc06b00f724a12bfde0c94dc613
Jeremy Corbett <jeremy.lael@gmail.com>
2011-08-13 20:35:01 -0400

    Thought that would of work, now to consult the docs

--- features/step_definitions/proc_process_steps.rb
+++ features/step_definitions/proc_process_steps.rb
@@ -1,6 +1,6 @@
 
 When /^I look at \/proc\/self\/status$/ do
-    pending # express the regexp above with the code you wish you had
+  @self_status.should_not_be :nil?
 end
 
 Then /^I should have some info about myself$/ do
