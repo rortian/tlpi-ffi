28f03aea93b2912cb0b0a698ac5318ca802bb126
Jeremy Corbett <jeremy.lael@gmail.com>
2011-08-13 20:16:14 -0400

    into pry

--- lib/proc/proc.rb
+++ lib/proc/proc.rb
@@ -1,10 +1,11 @@
 module TLPI::Proc
   class Process
 
-    def initialize
-
+    def initialize(pid = 'self')
+      @status_path = "/proc/#{pid}/status"
+      require 'pry'
+      binding.pry
     end
   end
 end
 
-puts 'hi!'
