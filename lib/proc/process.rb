bb5cdf87efffdda553d2c55172ade42f4d0ea59e
Jeremy Corbett <jeremy.lael@gmail.com>
2011-08-14 11:40:36 -0400

    Process now in it's own file

---
+++ lib/proc/process.rb
@@ -0,0 +1,34 @@
+module TLPI::Proc
+  class Process
+    attr_reader :name,:state,:tgid,:pid,:ppid,
+      :threads
+
+    def initialize(pid = 'self')
+      @status_path = "/proc/#{pid}/status"
+      check_status
+    end
+
+    def check_status
+      status = File.open @status_path
+      status.readlines.map(&:split).each do |(entry,*value)|
+        easy = value.first
+        case entry
+        when /Name:/
+          @name = easy
+        when /State:/
+          @state = easy
+        when /Tgid:/
+          @tgid = easy.to_i
+        when /^Pid:/
+          @pid = easy.to_i
+        when /^PPid:/
+          @ppid = easy.to_i
+        when /^Threads:/
+          @threads = easy.to_i
+        else
+          puts value.unshift(entry).join(' ')
+        end
+      end
+    end
+  end
+end
