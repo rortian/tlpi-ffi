008f9df4047156f4b5c64a32c5de0ab1c8c39ede
Jeremy Corbett <jeremy.lael@gmail.com>
2011-08-13 20:35:22 -0400

    Checking out the lines

--- lib/proc/proc.rb
+++ lib/proc/proc.rb
@@ -3,8 +3,18 @@ module TLPI::Proc
 
     def initialize(pid = 'self')
       @status_path = "/proc/#{pid}/status"
-      require 'pry'
-      binding.pry
+      check_status
+    end
+
+    def check_status
+      status = File.open @status_path
+      status.readlines.each do |line|
+        case line
+        when
+        else
+          puts line
+        end
+      end
     end
   end
 end
