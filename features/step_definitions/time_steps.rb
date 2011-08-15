86a86fa1bca22a6890698362481db3143db53d22
Jeremy Corbett <jeremy.lael@gmail.com>
2011-08-14 20:24:10 -0400

    Now pending

---
+++ features/step_definitions/time_steps.rb
@@ -0,0 +1,7 @@
+When /^I call to gettimeofday$/ do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should know what time it is$/ do
+    pending # express the regexp above with the code you wish you had
+end
