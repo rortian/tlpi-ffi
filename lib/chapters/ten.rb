0e5edcd4b2de07e70360909a2ba4d2e26785cba4
Jeremy Corbett <jeremy.lael@gmail.com>
2011-08-14 20:17:38 -0400

    Hmm maybe that will work

--- lib/chapters/ten.rb
+++ lib/chapters/ten.rb
@@ -3,5 +3,15 @@ module TLPI::Ten
   extend FFI::Library
   ffi_lib FFI::Library::LIBC
 
-  puts "Hello there!!"
+  #p 186
+  attach_function :gettimeofday, [:pointer,:pointer], :int
+
+  #p 186
+  #struct timeval
+  #time t tv_sec
+  #suseconds tv_usec
+  class Timeval < FFI::Struct
+    layout :tv_sec , :int,
+           :tv_usec, :int
+  end
 end
