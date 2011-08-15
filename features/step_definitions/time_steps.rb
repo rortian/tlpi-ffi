6c693072acf23e66f73ac637975000662711cea6
Jeremy Corbett <jeremy.lael@gmail.com>
2011-08-14 20:27:15 -0400

    No explosion yet

--- features/step_definitions/time_steps.rb
+++ features/step_definitions/time_steps.rb
@@ -1,5 +1,6 @@
 When /^I call to gettimeofday$/ do
-    pending # express the regexp above with the code you wish you had
+  @time_struct = TLPI::Ten::Timeval.new
+  @time_status = TLPI::Ten.gettimeofday @time_struct,nil
 end
 
 Then /^I should know what time it is$/ do
