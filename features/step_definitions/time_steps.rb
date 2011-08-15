7bcf5647951864ee40a8fb4b66e4136b8fa130e6
Jeremy Corbett <jeremy.lael@gmail.com>
2011-08-14 20:35:53 -0400

    Sweet, a passing test using a struct

--- features/step_definitions/time_steps.rb
+++ features/step_definitions/time_steps.rb
@@ -1,8 +1,10 @@
 When /^I call to gettimeofday$/ do
   @time_struct = TLPI::Ten::Timeval.new
   @time_status = TLPI::Ten.gettimeofday @time_struct,nil
+  @ruby_time = Time.now.to_i
 end
 
 Then /^I should know what time it is$/ do
-    pending # express the regexp above with the code you wish you had
+  comparison = @time_struct[:tv_sec] - @ruby_time
+  comparison.should < 2
 end
