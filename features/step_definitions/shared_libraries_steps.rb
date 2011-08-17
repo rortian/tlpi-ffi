a3d9c5db9507c1fd48d1d36a02518890a424befc
Jeremy Corbett <rortian@zeevex.com>
2011-08-17 01:17:29 -0400

    Diggin in

--- features/step_definitions/shared_libraries_steps.rb
+++ features/step_definitions/shared_libraries_steps.rb
@@ -1,8 +1,9 @@
 When /^I open a shared library$/ do
-  TLPI::FortyTwo.dlopen("xml2",TLPI::FortyTwo::RTLD_LAZY)
+  @pointer  = TLPI::FortyTwo.dlopen("xml2",TLPI::FortyTwo::RTLD_LAZY)
 end
 
 Then /^I should still be okay$/ do
-    pending # express the regexp above with the code you wish you had
+  require 'pry'
+  binding.pry
 end
 
