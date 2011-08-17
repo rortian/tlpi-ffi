784d6be970377dfe1179869080170ccd26433601
Jeremy Corbett <rortian@zeevex.com>
2011-08-17 00:51:15 -0400

    Constants and function that is not in libc

--- lib/chapters/forty-two.rb
+++ lib/chapters/forty-two.rb
@@ -3,6 +3,13 @@ module TLPI::FourtyTwo
   extend FFI::Library
   ffi_lib FFI::Library::LIBC
 
-  puts 'It\'s here'
+  RTLD_LAZY = 1 
+  RTLD_NOW  = 2
+  RTLD_BINDING_MASK = 3 
+  RTLD_NOLOAD = 5
+  RTLD_DEEPBIND = 8
+
+  #p860
+  attach_function :dlopen, [:string,:int], :pointer
 end
 
