4c88f0a37d9e53b380df384bb5b60c5a132670c3
Jeremy Corbett <rortian@zeevex.com>
2011-08-17 01:08:02 -0400

    Now pending

---
+++ features/step_definitions/shared_libraries_steps.rb
@@ -0,0 +1,8 @@
+When /^I open a shared library$/ do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should still be okay$/ do
+    pending # express the regexp above with the code you wish you had
+end
+
