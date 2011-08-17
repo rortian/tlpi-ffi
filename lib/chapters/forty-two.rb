f716e7aa76450dff5b2ab0e89ca5749d790d70ee
Jeremy Corbett <rortian@zeevex.com>
2011-08-17 00:40:12 -0400

    Starting on another chapter

---
+++ lib/chapters/forty-two.rb
@@ -0,0 +1,8 @@
+
+module TLPI::FourtyTwo
+  extend FFI::Library
+  ffi_lib FFI::Library::LIBC
+
+  puts 'It\'s here'
+end
+
--- lib/tlpi-ffi.rb
+++ lib/tlpi-ffi.rb
@@ -9,5 +9,6 @@ end
 require 'chapters/six'
 require 'chapters/seven'
 require 'chapters/ten'
+require 'chapters/forty-two'
 
 require 'proc/proc'
