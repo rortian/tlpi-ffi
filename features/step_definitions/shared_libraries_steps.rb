5971eaeb7cbadc5b1c51df7a1698edfc95ec6cab
Jeremy Corbett <rortian@zeevex.com>
2011-08-17 01:16:42 -0400

    Moving along

--- features/step_definitions/shared_libraries_steps.rb
+++ features/step_definitions/shared_libraries_steps.rb
@@ -1,5 +1,5 @@
 When /^I open a shared library$/ do
-    pending # express the regexp above with the code you wish you had
+  TLPI::FortyTwo.dlopen("xml2",TLPI::FortyTwo::RTLD_LAZY)
 end
 
 Then /^I should still be okay$/ do
